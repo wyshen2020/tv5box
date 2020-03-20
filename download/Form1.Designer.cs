bc312a5f7b3070f3980a13f87a51d0c66afcb678
wyshen2020 <wyshen2013@139.com>
2020-03-20 23:45:49 +0800

    init code

---
+++ download/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace download
+{
+    partial class download
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbFileName = new System.Windows.Forms.TextBox();
+            this.BtnBrowser = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tbLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // tbFileName
+            // 
+            this.tbFileName.Location = new System.Drawing.Point(0, 1);
+            this.tbFileName.Name = "tbFileName";
+            this.tbFileName.Size = new System.Drawing.Size(234, 21);
+            this.tbFileName.TabIndex = 0;
+            // 
+            // BtnBrowser
+            // 
+            this.BtnBrowser.Location = new System.Drawing.Point(244, 1);
+            this.BtnBrowser.Name = "BtnBrowser";
+            this.BtnBrowser.Size = new System.Drawing.Size(40, 21);
+            this.BtnBrowser.TabIndex = 1;
+            this.BtnBrowser.Text = "...";
+            this.BtnBrowser.UseVisualStyleBackColor = true;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(124, 28);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(70, 25);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(205, 28);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(70, 25);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tbLog
+            // 
+            this.tbLog.Location = new System.Drawing.Point(0, 59);
+            this.tbLog.Multiline = true;
+            this.tbLog.Name = "tbLog";
+            this.tbLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbLog.Size = new System.Drawing.Size(284, 163);
+            this.tbLog.TabIndex = 4;
+            // 
+            // download
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 222);
+            this.Controls.Add(this.tbLog);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.BtnBrowser);
+            this.Controls.Add(this.tbFileName);
+            this.Name = "download";
+            this.Text = "download";
+            this.Load += new System.EventHandler(this.download_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbFileName;
+        private System.Windows.Forms.Button BtnBrowser;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox tbLog;
+    }
+}
+
---
+++ download/Form1.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+using System.Threading;
+
+namespace download
+{
+    public partial class download : Form
+    {
+        public download()
+        {
+            InitializeComponent();
+        }
+
+        System.Threading.SynchronizationContext m_SyncContext = null;
+        private int _iTaskThis;
+        private List<KeyValuePair<string, string>> _tasks;
+
+        void TaskMain(object state)
+        {
+            const int iTaskSize = 50;
+            for (int i = 0; i < iTaskSize; i++)
+            {
+                ThreadPool.QueueUserWorkItem(TaskWork, i);
+            }
+            while (true)
+            {
+                ThreadRunForMethodOne("This is task main");
+                Thread.Sleep(1000);
+            }
+        }
+
+        void TaskWork(object state)
+        {
+            while (true)
+            {
+                var text = string.Format("This is task {0}", state);
+                ThreadRunForMethodOne(text);
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void download_Load(object sender, EventArgs e)
+        {
+            var filename = "default.xml";
+            if (File.Exists(filename))
+            {
+                tbFileName.Text = filename;
+            }
+
+            var lines = new string[20];
+            for (int i = 0; i < lines.Length; i++)
+            {
+                lines[i] = "text " + i.ToString();
+            }
+            tbLog.Lines = lines;
+
+            ThreadPool.SetMinThreads(5, 5);
+            ThreadPool.SetMaxThreads(50, 50);
+            m_SyncContext = SynchronizationContext.Current;
+        }
+
+        private void ThreadRunForMethodOne(object state)
+        {
+            m_SyncContext.Post(SendOrPostCallback, state);
+        }
+
+        void SendOrPostCallback(object state)
+        {
+            tbLog.AppendText((string)state + "\r\n");
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            var filename = tbFileName.Text;
+            if (!File.Exists(filename))
+            {
+                MessageBox.Show("file not exist");
+                return;
+            }
+
+            var doc = new XmlDocument();
+            doc.Load(filename);
+
+            _tasks = new List<KeyValuePair<string, string>>();
+            foreach (var node in doc.DocumentElement)
+            {
+                if (node is XmlElement)
+                {
+                    var element = node as XmlElement;
+                    _tasks.Add(new KeyValuePair<string, string>(element.GetAttribute("name"), element.GetAttribute("uri")));
+                }
+            }
+
+            if (_tasks.Count > 0)
+            {
+                var btn = sender as Button;
+                btn.Enabled = false;
+                _iTaskThis = 0;
+                ThreadPool.QueueUserWorkItem(TaskMain);
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
